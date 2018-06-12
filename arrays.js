97065bf70df3dc502e36fca016d587e1123a5f50
Nick Romans <nicholas.romans@gmail.com>
2018-06-12 01:09:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,44 +1,9 @@
-var chocolateBars = [
-  "snickers", "hundred grand", "kitkat", "skittles"
-  ];
-  
-  function addElementToBeginningOfArray(chocolateBars, Hersheys){
-    return [Hersheys, ...chocolateBars]
-  ;}
-  
-  function destructivelyAddElementToBeginningOfArray(chocolateBars,junk){
-    chocolateBars.unshift(junk);
-    return chocolateBars
-  ;}
-  
-  function addElementToEndOfArray(chocolateBars, Garbage){
-    return [...chocolateBars, Garbage]
-  ;}
-  
-  function destructivelyAddElementToEndOfArray(chocolateBars, crap){
-    chocolateBars.push(crap);
-    return chocolateBars
-  ;}
-  
-  function removeElementFromBeginningOfArray(chocolateBars){
-    return chocolateBars.slice(1)
-  ;}
-  
-  function destructivelyRemoveElementFromEndOfArray(chocolateBars){
-    chocolateBars.pop();
-    return(chocolateBars)
-  ;}
-  
-  function removeElementFromEndOfArray(chocolateBars){
-    return chocolateBars.slice(0, chocolateBars.length -1)
-    ;}
-    
-    function accessElementInArray(chocolateBars, kitkat) {
- return chocolateBars[kitkat]; 
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(chocolateBars, element){
+  return["Goodbar", ...chocolateBars];
 }
-function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
-  chocolateBars.shift()
-  return chocolateBars
+function destructivelyAddElementToBeginningOfArray(chocolateBar, element){
+chocolateBars.unshift('wonka');
+return chocolateBars;
 }
-  
-  
